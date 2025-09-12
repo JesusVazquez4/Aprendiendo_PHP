c793c611f7e936416426ab6fa10fdeed098c997a
JesusVazquez4 <jesus.wesker4321@gmail.com>
2025-09-12 13:25:57 -0600

    Se llega hasta Slicing Strings

--- Principios/inicios/strings.php
+++ Principios/inicios/strings.php
@@ -35,5 +35,70 @@
         echo "<p>$nombre_1</p>";
         echo str_word_count($nombre_1);
     ?>
+    <h2><code>strpos</code></h2>
+    <?php
+        $x = "Jesús Vázquez";
+        echo "<p>$x</p><br>";
+        echo strpos($x, "Vázquez");
+        echo "<br>" . strpos($x, "Jesús");
+    ?>
+    <h2><code>strtoupper</code></h2>
+    <?php
+        $x = "Jesús Vázquez";
+        echo "<p>$x</p><br>";
+        echo strtoupper($x);
+    ?>
+    <h2><code>strtolower</code></h2>
+    <?php
+        $x = "Jesús Vázquez";
+        echo "<p>$x</p>";
+        echo strtolower($x);
+        $y = "JESUS VAZQUEZ";
+        echo "<p>$y</p>";
+        echo strtolower($y);
+    ?>
+    <h2><code>str_replace</code></h2>
+    <?php
+        $x = "Hola Mundo";
+        echo "<p>$x</p>";
+        echo str_replace("Mundo", "Jesus", $x);
+    ?>
+    <h2><code>strrev</code></h2>
+    <?php
+        $x = "Jesús Vázquez";
+        echo "<p>$x</p>";
+        echo strrev($x);
+        $y = "Jesus Vazquez";
+        echo "<p>$y</p>";
+        echo strrev($y);
+    ?>
+    <h2><code>trim</code></h2>
+    <?php
+        $x = "   Jesús Vázquez   ";
+        echo "<input type='text' value='". $x ."'>";
+        echo "<br>";
+        echo "<input type='text' value='" . trim($x) . "'>";
+    ?>
+    <h2><code>explode</code></h2>
+    <?php
+        $x = "Jesús Vázquez";
+        $y = explode(" ", $x);
+        print_r($y);
+        echo("<br>");
+        var_dump($y);
+    ?>
+    <h2><code>substr</code></h2>
+    <?php
+        $x = "Curso de PHP";
+        echo substr($x, 4, 3);
+        echo "<br>";
+        echo substr($x, 4);
+        echo "<br>";
+        echo substr($x, -5, 3);
+        echo "<br>";
+        echo substr($x, -5);
+        echo "<br>";
+        echo substr($x, 2, -3);
+    ?>
 </body>
 </html>
